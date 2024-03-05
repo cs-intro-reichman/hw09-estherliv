efb72bceb06a0891884d2a2c0c697bad4270f304
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2024-03-05 21:13:20 +0200

    Update List.java

--- List.java
+++ List.java
@@ -1,5 +1,3 @@
-import java.util.ListIterator;
-
 /** A linked list of character data objects.
  *  (Actually, a list of Node objects, each holding a reference to a character data object.
  *  However, users of this class are not aware of the Node objects. As far as they are concerned,
